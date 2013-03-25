139d47a45e2ef058f8c84b8b5301226359fb891b
ema <ema@sfc.wide.ad.jp>
2013-03-25 12:59:51 +0900

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+var contentFadeInTime = 700;
+var contentFadeOutTime = 700;
+var contentJsonUrl = "http://localhost/wint/content.json";
+
+var newsEffectSpeed = 1000;
+var newsSwitchDelay = 10000;
+var newsEasing = "swing";
+var newsJsonUrl = "http://localhost/wint/news.json";
